46c80d0306844b40724b821f59870df848e63253
Gerard Keown <gerard.keown@pytilia.io>
2022-04-26 09:40:31 +0100

    End of section 2

---
+++ Domain/Activity.cs
@@ -0,0 +1,14 @@
+namespace Domain
+{
+    public class Activity
+    {
+        // GUID is a globally unique identifier - Id means primary key in entity framework
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string City { get; set; }
+        public string Venue { get; set; }
+    }
+}
